f84735dae570cc3491300c53b962c52ec1d264fd
Yulia Zasinets <yulia.zasinets@oxagile.com>
2021-08-17 18:06:10 +0300

    Task 2

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ const readline = require('readline').createInterface({
     output: process.stdout
 });
 
+class User {
+    constructor (name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    // userGender() {
+    //     if (/[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1))) {
+    //         this.gender = "woman";
+    //     } else {
+    //         this.gender = "man";
+    //     }
+    // }
+
+    sayHello() {
+        (this.name.length < 3) ? console.log('Имя должно содержать более 3-х букв') :
+            (this.name.includes(' ')) ? console.log(`Привет, ${this.name.split(' ').join('').toUpperCase()}`) :
+                (/\d/.test(this.name)) ? console.log('Имя не может содержать цифры') :
+                    (/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.name)) ? console.log('Имя не может содержать символы') :
+                        (/[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1))) ? console.log(`Привет, ${this.name[0].toUpperCase() + this.name.slice(1).toLowerCase()}! Ты такая красивая сегодня!`) :
+                            console.log(`Привет, ${this.name[0].toUpperCase() + this.name.slice(1).toLowerCase()}! Ты такой красивый сегодня!`);
+    }
+}
+
 readline.question(`Введите Ваше имя: `, function(name) {
-    console.log(`Привет, ${name.toUpperCase()}`);
+    new User(name).sayHello();
     readline.close();
 });
