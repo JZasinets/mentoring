9208e330c1d893d23a6abf57dea71db0c58d0238
Yulia Zasinets <yulia.zasinets@oxagile.com>
2021-08-18 12:30:43 +0300

    Task 2. Changes

--- index.js
+++ index.js
@@ -9,23 +9,26 @@ class User {
     }
 
     greeting = (name) => {
-        this.name = name;
-        const isNameContainsSpaces = this.name.includes(' ');
-        const isWomanName = /[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1));
+        this.name = "Имя не распозано";
+        this.gender = "Гендер не распозан";
+        const isNameContainsSpaces = name.includes(' ');
+        const isWomanName = /[aeiouаоиеёэыуюя]/g.test(name.slice(-1));
+        this.gender = isWomanName ? "woman" : "man";
 
-        if (isNameContainsSpaces) this.name = this.name.split(' ').join('');
-        if (!this.name) return console.log('Имя не может быть пустым');
-        if (isWomanName) this.gender = "woman";
+        if (isNameContainsSpaces) this.name = name.split(' ').join('');
+        if (!name) return console.log('Имя не может быть пустым');
+        this.name = name;
 
         const isNameSmall = this.name.length < 3;
         const isNameContainsNumbers = /\d/.test(this.name);
         const isNameContainsSymbols = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.name);
         const showUserName = this.name[0].toUpperCase() + this.name.slice(1).toLowerCase();
+        console.log(`Ваш пол ${this.gender}`)
 
         if (isNameSmall) return console.log('Имя должно содержать более 3-х букв');
         if (isNameContainsNumbers) return console.log('Имя не может содержать цифры');
         if (isNameContainsSymbols) return console.log('Имя не может содержать символы');
-        if (this.gender) return console.log(`Привет, ${showUserName}! Ты такая красивая сегодня!`);
+        if (isWomanName) return console.log(`Привет, ${showUserName}! Ты такая красивая сегодня!`);
         return console.log(`Привет, ${showUserName}! Ты такой красивый сегодня!`);
     }
 }
