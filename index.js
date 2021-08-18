a17540d869af7ba22393d5ffaf44206b71ec6e5c
Yulia Zasinets <yulia.zasinets@oxagile.com>
2021-08-18 11:47:44 +0300

    Task 2. Changes

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ const readline = require('readline').createInterface({
 });
 
 class User {
-    greeting = (name, gender) => {
-        this.name = name;
+    constructor (gender) {
         this.gender = gender;
+    }
 
+    greeting = (name) => {
+        this.name = name;
         const isNameContainsSpaces = this.name.includes(' ');
+        const isWomanName = /[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1));
 
         if (isNameContainsSpaces) this.name = this.name.split(' ').join('');
         if (!this.name) return console.log('Имя не может быть пустым');
+        if (isWomanName) this.gender = "woman";
 
         const isNameSmall = this.name.length < 3;
         const isNameContainsNumbers = /\d/.test(this.name);
         const isNameContainsSymbols = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.name);
-        const isWomanName = /[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1));
         const showUserName = this.name[0].toUpperCase() + this.name.slice(1).toLowerCase();
 
         if (isNameSmall) return console.log('Имя должно содержать более 3-х букв');
         if (isNameContainsNumbers) return console.log('Имя не может содержать цифры');
         if (isNameContainsSymbols) return console.log('Имя не может содержать символы');
-        if (isWomanName) return console.log(`Привет, ${showUserName}! Ты такая красивая сегодня!`);
+        if (this.gender) return console.log(`Привет, ${showUserName}! Ты такая красивая сегодня!`);
         return console.log(`Привет, ${showUserName}! Ты такой красивый сегодня!`);
     }
 }
