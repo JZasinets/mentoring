e13b8456d8389f9507666cb3fd4adb8ac455d580
Yulia Zasinets <yulia.zasinets@oxagile.com>
2021-08-18 11:30:16 +0300

    Task 2. Changes

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ const readline = require('readline').createInterface({
 });
 
 class User {
-    constructor (name, gender) {
+    greeting = (name, gender) => {
         this.name = name;
         this.gender = gender;
-    }
 
-    // userGender() {
-    //     if (/[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1))) {
-    //         this.gender = "woman";
-    //     } else {
-    //         this.gender = "man";
-    //     }
-    // }
+        const isNameContainsSpaces = this.name.includes(' ');
 
-    sayHello() {
-        (this.name.length < 3) ? console.log('Имя должно содержать более 3-х букв') :
-            (this.name.includes(' ')) ? console.log(`Привет, ${this.name.split(' ').join('').toUpperCase()}`) :
-                (/\d/.test(this.name)) ? console.log('Имя не может содержать цифры') :
-                    (/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.name)) ? console.log('Имя не может содержать символы') :
-                        (/[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1))) ? console.log(`Привет, ${this.name[0].toUpperCase() + this.name.slice(1).toLowerCase()}! Ты такая красивая сегодня!`) :
-                            console.log(`Привет, ${this.name[0].toUpperCase() + this.name.slice(1).toLowerCase()}! Ты такой красивый сегодня!`);
+        if (isNameContainsSpaces) this.name = this.name.split(' ').join('');
+        if (!this.name) return console.log('Имя не может быть пустым');
+
+        const isNameSmall = this.name.length < 3;
+        const isNameContainsNumbers = /\d/.test(this.name);
+        const isNameContainsSymbols = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(this.name);
+        const isWomanName = /[aeiouаоиеёэыуюя]/g.test(this.name.slice(-1));
+        const showUserName = this.name[0].toUpperCase() + this.name.slice(1).toLowerCase();
+
+        if (isNameSmall) return console.log('Имя должно содержать более 3-х букв');
+        if (isNameContainsNumbers) return console.log('Имя не может содержать цифры');
+        if (isNameContainsSymbols) return console.log('Имя не может содержать символы');
+        if (isWomanName) return console.log(`Привет, ${showUserName}! Ты такая красивая сегодня!`);
+        return console.log(`Привет, ${showUserName}! Ты такой красивый сегодня!`);
     }
 }
 
-readline.question(`Введите Ваше имя: `, function(name) {
-    new User(name).sayHello();
+let user = new User();
+
+readline.question(`Введите Ваше имя: `, (name) => {
+    user.greeting(name);
     readline.close();
 });
