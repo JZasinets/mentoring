dc1865deff250521f59fe5887ed1b57fd7a34925
Yulia Zasinets <yulia.zasinets@oxagile.com>
2021-09-03 11:37:36 +0300

    Task 5. Fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,27 +1,30 @@
-const process = require('process');
+const readline = require('./readline');
 
-let fibonacciNumbers = (onReturnCommand) => {
-    let a = 1;
-    let b = 1;
+class FibonacciNumbers {
+    backCommands = (onReturnCommand, stopFibonacci) => {
+            let a = 0;
+            let b = 1;
 
-    let timerFibonacci = setInterval(() => {
-        let c = a + b;
-        a = b;
-        b = c;
-        console.log(b);
-        // if(...) {
-        //     clearInterval(timerFibonacci);
-        //     return onReturnCommand();
-        // }
-    }, 1000);
+            let timeFibonacci = setInterval(() => {
+                let c = a + b;
+                a = b;
+                b = c;
+                console.log(b);
+                readline.question('', (stopFibonacci) => {
+                    if(stopFibonacci === 'стоп') {
+                        clearInterval(timeFibonacci);
+                        onReturnCommand();
+                    }
+                })
+            }, 1000);
+
+    }
 }
 
+let fibonacciNumbers = new FibonacciNumbers();
+
 const commandFibonacci = (onReturnCommand) => {
-    fibonacciNumbers(onReturnCommand);
-    process.openStdin().on('SIGINT', () => {
-        console.log('Process stopped');
-        return onReturnCommand();
-    })
+        fibonacciNumbers.backCommands(onReturnCommand);
 }
 
 module.exports = commandFibonacci;
